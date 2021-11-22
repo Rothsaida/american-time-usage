74de9c26259d79c32eba4ae1931ad0c9e7116880
Bridger Gordon <bridgergordon@college.harvard.edu>
2021-11-22 16:11:13 -0500

    updated dot plot

--- Template/Template/js/avgHoursWorked.js
+++ Template/Template/js/avgHoursWorked.js
@@ -16,10 +16,10 @@ class LineChart {
     initVis(){
         let vis = this;
 
-        vis.margin = {top: 30, right: 10, bottom: 50, left: 20};
+        vis.margin = {top: 30, right: 100, bottom: 50, left: 120};
 
-        vis.width = 400 - vis.margin.left - vis.margin.right;
-        vis.height = 400 - vis.margin.top - vis.margin.bottom;
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height  - vis.margin.top - vis.margin.bottom;
         console.log(vis.width)
         console.log(vis.height)
         // SVG drawing area
@@ -30,9 +30,13 @@ class LineChart {
             .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
         // Scales and axes
+        vis.percentileScale = d3.scaleLinear()
+            .range([0, 100])
+            .domain([1, 16]);
+
         vis.x = d3.scaleLinear()
             .range([0, vis.width])
-            .domain([0, 17]);
+            .domain([-3, 103]);
 
         vis.y = d3.scaleLinear()
             .range([vis.height,0])
@@ -79,8 +83,8 @@ class LineChart {
             .enter()
             .append("circle")
             .attr("cx", function (d) {
-                console.log(vis.x(d.FAMINCOME));
-                return vis.x(d.FAMINCOME); } )
+                console.log(vis.x(vis.percentileScale(d.FAMINCOME)));
+                return vis.x(vis.percentileScale(d.FAMINCOME)); } )
             .attr("cy", function (d) {
                 console.log(vis.y(d.avgHours));
                 return vis.y(d.avgHours); } )
@@ -90,5 +94,24 @@ class LineChart {
         // Call axis functions with the new domain
         vis.svg.select(".x-axis").call(vis.xAxis);
         vis.svg.select(".y-axis").call(vis.yAxis);
+
+        vis.svg.append("text")
+            .attr("class", "x label")
+            .attr("text-anchor", "end")
+            .attr("x", vis.width)
+            .attr("y", vis.height - 6)
+            .attr("font-size", "10px")
+            .text("income percentile (from 0 to 100)");
+
+        vis.svg.append("text")
+            .attr("class", "y label")
+            .attr("text-anchor", "end")
+            .attr("y", -35)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .attr("font-size", "10px")
+            .text("hours worked per week");
+
+
     }
 }
