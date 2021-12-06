4bbf24e32540a911e2828a7b30c1f239ccec92b0
HalleClottey <halleclottey@gmail.com>
2021-12-06 11:43:20 -0500

    Fixed formatting and conclusion

--- Template/Template/js/clockChart.js
+++ Template/Template/js/clockChart.js
@@ -33,14 +33,14 @@ class ClockChart {
         let vis = this;
 
         // margin conventions
-        vis.margin = {top: 10, right: 50, bottom: 10, left: 50};
+        vis.margin = {top: 60, right: 50, bottom: 30, left: 50};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
             .attr("width", vis.width + vis.margin.left + vis.margin.right)
-            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom + 60)
             .append("g")
             .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
@@ -116,7 +116,7 @@ class ClockChart {
                     .style("left", e.pageX + 20 + "px")
                     .style("top", e.pageY + "px")
                     .html(`
-                         <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 20px">
+                         <div style="border: thin solid black; border-radius: 5px; background: white; padding: 20px; font-family: Constantia">
                              <h3>${vis.codeToActivity(d.data.activityTopLevel)}<h3>
                              <h4> Duration: ${d.data.DURATION.toFixed(2)} mins</h4>                       
                          </div>`);
